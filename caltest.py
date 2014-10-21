f3e5a8a99c6447070611a28f6be1c30100605f20
Dave Parsons <dualrudder@gmail.com>
2014-10-20 20:47:53 -0700

    Working on schedule

--- caltest.py
+++ caltest.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
-import datetime
+import logging
 import urllib2
 import json
-from dateutil import parser
+from dateutil import parser, zoneinfo, tz
+from datetime import datetime, timedelta
 
-def get_schedule():
-  url = ('https://spreadsheets.google.com/feeds/list/'
-    '1kGVn93ceoVzeOcern7KuixuDSfPJFJNN_gIDGM7Bc_g/od6/public/values?alt=json')
+tz_abbr = {
+  'ET': 'America/New_York',
+  'CT': 'America/Chicago',
+  'MT': 'America/Denver',
+  'RT': 'America/Phoeniz',
+  'PT': 'America/Los_Angeles',
+  'AT': 'America/Anchorage',
+  'HT': 'Pacific/Honolulu',
+}
+tzi = dict([(k, zoneinfo.gettz(v)) for k,v in tz_abbr.iteritems()])
+
+def get_schedule(schedule_id):
+  url = 'https://spreadsheets.google.com/feeds/list/%s/od6/public/values?alt=json' % schedule_id
   try:
     response = urllib2.urlopen(url)
     content = response.read()
   except urllib2.URLError:
-    print 'Error: could not retrieve data'
-    return
+    logging.warning('Warning: could not retrieve spreadsheet data for %s' % schedule_id)
+    return False
 
   try:
     data = json.loads(content)
   except ValueError:
-    print 'Error: invalid JSON format'
-    return
-
-  schedule = []
-  current_schedule = []
+    logging.error('Error: invalid JSON format for spreadsheet %s' % schedule_id)
+    return False
 
   if 'feed' not in data or 'entry' not in data['feed']:
-    print 'Error: invalid data format'
-    return
+    logging.warning('Warning: invalid data format for %s' % schedule_id)
+    return False
 
   data_keys = [
     ('gsx$day', 'day'),
@@ -34,44 +42,64 @@ def get_schedule():
   ]
 
   # Process entries in spreadsheet
+  schedule = []
   for entry in data['feed']['entry']:
     result = {}
     for entry_key, result_key in data_keys:
       if entry_key not in entry or '$t' not in entry[entry_key]:
-        print 'Error: key not found', entry_key
-        return
+        logging.warning('Warning: key not found for %s: %s' % (schedule_id, entry_key))
+        return False
       result[result_key] = entry[entry_key]['$t']
 
     day_time = '%s %s' % (result['day'], result['time'])
     try:
       result['datetime'] = parser.parse(day_time)
     except ValueError:
-      print 'Error: invalid time format', day_time
-      return
+      logging.warning('Warning: invalid time format for %s: %s' % (schedule_id, day_time))
+      return False
     try:
       result['temperature'] = int(result['temperature'])
     except ValueError:
-      print 'Error: invalid temperature', result['temperature']
-      return
+      logging.warning('Warning: invalid temperature format for %s: %s'
+          % (schedule_id, result['temperature']))
+      return False
 
     schedule.append({'d': result['day'], 't': result['time'], 'e': result['temperature']})
-    current_schedule.append({'dt': result['datetime'], 'e': result['temperature']})
+
+  return json.dumps(schedule, separators=(',', ':'))
+
+def normalize(dt_str):
+  dt = parser.parse(dt_str, tzinfos=tzi).astimezone(tz.tzutc()).replace(tzinfo=None)
+
+  # Normalize each datetime to within one week from now
+  now = datetime.utcnow()
+  oneweek = timedelta(days=7)
+  oneweek_from_now = now + oneweek
+  while dt < now:
+    dt += oneweek
+  while dt > oneweek_from_now:
+    dt -= oneweek
+
+  return dt
+
+def get_next_event(schedule):
+  schedule = json.loads(schedule)
+  current_schedule = [{
+    'dt': parser.parse('%s %s' % (entry['d'], entry['t'])) + timedelta(hours=-17),
+    'e': entry['e'],
+  } for entry in schedule]
 
   current_schedule.sort(key=lambda x:x['dt'])
-  now = datetime.datetime.now()
+  now = datetime.utcnow()
+  print now
   # Rotate list until next entry is first
   while current_schedule[0]['dt'] < now:
     entry = current_schedule.pop(0)
-    entry['dt'] += datetime.timedelta(days=7)
+    entry['dt'] += timedelta(days=7)
     current_schedule.append(entry)
 
-  for entry in current_schedule:
-    print entry['dt'], entry['e']
+  return current_schedule[-1]['e'], current_schedule[0]['dt']
 
-  print 'Current set temp:', current_schedule[-1]['e']
-  print 'Next set temp:', current_schedule[0]['e'], 'at', current_schedule[0]['dt']
-
-  print json.dumps(schedule, separators=(',', ':'))
 
 # TODO: Store spreadsheet id, schedule, current set temp, next set temp/time
 #
@@ -84,4 +112,10 @@ def get_schedule():
 #   set_temp = last_event.temp
 #   store next_event
 
-get_schedule()
+# schedule = get_schedule('1kGVn93ceoVzeOcern7KuixuDSfPJFJNN_gIDGM7Bc_g')
+# if not schedule:
+#   print 'Failed'
+# print 'Schedule: ' + schedule
+# print get_next_event(schedule)
+
+print normalize('Monday 10pm PT')
